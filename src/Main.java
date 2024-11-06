e6ecf4394b979d57119e4f159a615f49cb49f976
/artem kasapov <kasapova79@gmail.com>
2024-11-06 14:50:49 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,153 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Dev. artem.kasapov (@Nebulart-dev) - Java//
+
+//Месяц и количество дней
+import java.util.Scanner; //нужен для ввода данных в консоле
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Число месяца: ");
+        int month = in.nextInt();
+        switch (month) {
+            case 1:
+                System.out.println("Январь: 31 день ");
+                break;
+            case 2:
+                System.out.println("Февраль: 28 дней ");
+                break;
+            case 3:
+                System.out.println("Март: 31 день ");
+                break;
+            case 4:
+                System.out.println("Апрель: 30 день ");
+                break;
+            case 5:
+                System.out.println("Май: 31 день ");
+                break;
+            case 6:
+                System.out.println("Июнь: 30 дней ");
+                break;
+            case 7:
+                System.out.println("Июль: 31 дней ");
+                break;
+            case 8:
+                System.out.println("Август: 31 день ");
+                break;
+            case 9:
+                System.out.println("Сентябрь: 30 дней");
+                break;
+            case 10:
+                System.out.println("Октябрь: 31 день");
+                break;
+            case 11:
+                System.out.println("Ноябрь: 30 дней");
+                break;
+            case 12:
+                System.out.println("Декабрь: 31 день");
+                break;
+            default:
+                System.out.println("Неизвестное число -> " + month);
         }
     }
 }
+
+//Перевод чисел в слова (без ввода в консоли)
+/*
+public class Main {
+    public static void main(String[] args) {
+        int num = 4;
+        switch (num) {
+            case 1:
+                System.out.println("Один");
+                break;
+            case 2:
+                System.out.println("Два");
+                break;
+            case 3:
+                System.out.println("Три");
+                break;
+            case 4:
+                System.out.println("Четыре");
+                break;
+            case 5:
+                System.out.println("Пять");
+                break;
+            case 6:
+                System.out.println("Шесть");
+                break;
+            case 7:
+                System.out.println("Семь");
+                break;
+            case 8:
+                System.out.println("Восемь");
+                break;
+            case 9:
+                System.out.println("Девять");
+                break;
+            default:
+                System.out.println("Ошибка!");
+        }
+    }
+}
+
+ */
+
+//Определение части суток
+/*
+import java.util.Scanner; //нужен для считывания данных с консоли
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Время: ");
+        int month = in.nextInt();
+
+        if (month >= 0 && month <= 5) {
+            System.out.print("Ночь");
+        } else if (month >= 6 && month <= 11) {
+            System.out.print("Утро");
+        } else if (month >= 12 && month <= 17) {
+            System.out.print("День");
+        } else if (month >= 18 && month <= 23) {
+            System.out.print("Вечер");
+        } else {
+            System.out.println("Некорректное число -> " + month);
+        }
+    }
+}
+*/
+
+//Определение типа фигуры по числу
+/*
+import java.util.Scanner; //нужен для считывания данных с консоли
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число от 3 до 6: ");
+        int shapeNumber = in.nextInt();
+
+        if (shapeNumber < 3 || shapeNumber > 6) {
+            System.out.println("> Недопустимое число");
+        } else {
+            switch (shapeNumber) {
+                case 3:
+                    System.out.println("> Треугольник");
+                    break;
+                case 4:
+                    System.out.println("> Четырёхугольник");
+                    break;
+                case 5:
+                    System.out.println("> Пятиугольник");
+                    break;
+                case 6:
+                    System.out.println("> Шестиугольник");
+                    break;
+                default:
+                    System.out.println("> Ошибка ввода");
+            }
+        }
+    }
+}
+
+ */
